b538a91d70e9297c56fae6befd1271d8477a5dc0
MalharPatel <malharpatel97@gmail.com>
2015-09-26 12:14:34 -0700

    data exchange works
    different html

--- hello.py
+++ hello.py
@@ -1,19 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('message.html')
-
-@app.route('/signup', methods = ['POST'])
-def signup():
-    relationship = request.form['Relationship']
-    family = request.form['Family']
-    work = request.form['Work']
-    print(relationship, family, work)
-    return redirect('/')
+def aboutlandingpage():
+    return render_template('about.html')
 
 
-if __name__ == '__main__':
+@app.route('/Feed', methods = ['GET', 'POST'])
+def getAd():
+    if request.method == 'POST':
+        selection = request.form['Events']
+
+
+    #call selection on function that back-end people write and return relevant information
+
+    return render_template("getAd.html", selection=selection)
+
+
+
+
+
+
+if __name__ == "__main__":
     app.run()
 
+
+
+
+
+#@app.route('/results.html', methods=['GET','POST'])
+#def results():
+#    g.db = connect_db()
+#
+#    cur = g.db.execute("SELECT * FROM all_items WHERE name = '{}'".format('Red'))
+#    posts = [dict(item=row[0], name=row[1]) for row in cur.fetchall()]
+#    g.db.close()
+#    return render_template('results.html', posts=posts)
